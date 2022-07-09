701b9fa328ba52a54d435474eee3aed6ecc78bf5
Alireza Haghighat <alireza.haghighat@zalando.de>
2022-07-09 16:20:37 +0200

    Check `is_short` and `leverage` fields when `is_future` is `true`.

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -25,6 +26,13 @@ def get_db():
 def create_signal(signal: schemas.SignalCreate, db: Session = Depends(get_db)):
     signal.received_at = datetime.now()
     signal.status = 0
+    if signal.is_futures:
+        if signal.is_short is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Field 'is_short' is mandatory when signal is future!")
+        if signal.leverage is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Field 'leverage' is mandatory when signal is future!")
     return crud.create_signal(db=db, signal=signal)
 
 
@@ -38,6 +46,5 @@ def read_signals(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 def read_signal(signal_id: int, db: Session = Depends(get_db)):
     db_signal = crud.get_signal(db, signal_id=signal_id)
     if db_signal is None:
-        raise HTTPException(status_code=404, detail="Signal not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Signal not found")
     return db_signal
-
